fc67a2350798aca20550108c70d734a91b88c13f
Adegoke Joshua Oluwafemi <femijay9@gmail.com>
2020-11-27 23:09:29 +0100

    updated product card

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-const ProductCard = () => {
+const ProductCard = ({ id, image_url, title, price, addToCart, currency }) => {
     return (
-        <div>
-
+        <div className="product">
+            <img src={image_url} alt={`${title}`} />
+            <h4>{title}</h4>
+            <p>From {currency} {price}</p>
+            <button onClick={() => addToCart(id, title, image_url, price)}>Add to Cart</button>
         </div>
     )
 }
