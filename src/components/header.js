3b37e17de3df744b52b3121b40428161e4df2acf
Adegoke Joshua Oluwafemi <femijay9@gmail.com>
2020-12-03 19:27:57 +0100

    header currency update

--- src/components/header.js
+++ src/components/header.js
@@ -3,19 +3,17 @@ import { logo, cartIcon } from '../assets';
 
 const Header = ({ cartCount, setSidebarShow }) => {
     return (
-        <>
-            <header className="flex">
-                <img src={logo} alt="logo" />
-                <p>Shop</p>
-                <p>Learn</p>
+        <header className="flex">
+            <img src={logo} alt="logo" />
+            <p>Shop</p>
+            <p>Help</p>
+            <p>Blog</p>
 
-                <div className="flex">
-                    <p>Account</p>
-                    <p onClick={() => setSidebarShow(true)}><img src={cartIcon} alt="Cart icon" /> {cartCount}</p>
-                </div>
-            </header>
-
-        </>
+            <div className="flex">
+                <p>Account</p>
+                <p onClick={() => setSidebarShow(true)}><img src={cartIcon} alt="Cart icon" /> {cartCount}</p>
+            </div>
+        </header>
     )
 }
 
